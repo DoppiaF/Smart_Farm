4532f5b7e84f583fb08ce20a048107a88ae4fb87
Francesco Furini <francesco.furini@edu.unife.it>
2024-08-05 00:07:23 +0200

    aggiunto model object 'utente' che mappa la tabella mysql

---
+++ main/java/com/university/project/Main.java
@@ -0,0 +1,22 @@
+package Smart_Farm.main.java.com.university.project;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Eseguiamo la GUI in un thread separato per garantire thread-safety
+        SwingUtilities.invokeLater(() -> {
+            // Creiamo la finestra principale
+            JFrame frame = new JFrame("My First Swing Application");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 300);
+            
+            // Creiamo un'etichetta e la aggiungiamo al frame
+            JLabel label = new JLabel("Hello, Swing!", SwingConstants.CENTER);
+            frame.getContentPane().add(label);
+
+            // Rendi la finestra visibile
+            frame.setVisible(true);
+        });
+    }
+}

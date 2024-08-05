8fd6f70f418ebd186a36c9180992a9491f71adc4
Francesco Furini <francesco.furini@edu.unife.it>
2024-08-05 20:11:39 +0200

    aggiunto 'zona' come entità nei Model Object

---
+++ src/java/com/unife/project/model/mo/VisitaVeterinaria.java
@@ -0,0 +1,81 @@
+package com.unife.project.model.mo;
+
+import java.time.LocalDateTime;
+
+public class VisitaVeterinaria {
+    private LocalDateTime data;     //PK
+    private String diagnosi;
+    private int identificativoAnimale; //FK di animale  //PK
+    private LocalDateTime prossimaVisita;
+    private String nomeVeterinario;
+    private String cognomeVeterinario;
+    private String curaPrescritta;
+
+    public VisitaVeterinaria(LocalDateTime data, String diagnosi, int identificativoAnimale, LocalDateTime prossimaVisita, String nomeVeterinario, String cognomeVeterinario, String curaPrescritta) {
+        this.data = data;
+        this.diagnosi = diagnosi;
+        this.identificativoAnimale = identificativoAnimale;
+        this.prossimaVisita = prossimaVisita;
+        this.nomeVeterinario = nomeVeterinario;
+        this.cognomeVeterinario = cognomeVeterinario;
+        this.curaPrescritta = curaPrescritta;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getDiagnosi() {
+        return diagnosi;
+    }
+
+    public void setDiagnosi(String diagnosi) {
+        this.diagnosi = diagnosi;
+    }
+
+    public int getIdentificativoAnimale() {
+        return identificativoAnimale;
+    }
+
+    public void setIdentificativoAnimale(int identificativoAnimale) {
+        this.identificativoAnimale = identificativoAnimale;
+    }
+
+    public LocalDateTime getProssimaVisita() {
+        return prossimaVisita;
+    }
+
+    public void setProssimaVisita(LocalDateTime prossimaVisita) {
+        this.prossimaVisita = prossimaVisita;
+    }
+
+    public String getNomeVeterinario() {
+        return nomeVeterinario;
+    }
+
+    public void setNomeVeterinario(String nomeVeterinario) {
+        this.nomeVeterinario = nomeVeterinario;
+    }
+
+    public String getCognomeVeterinario() {
+        return cognomeVeterinario;
+    }
+
+    public void setCognomeVeterinario(String cognomeVeterinario) {
+        this.cognomeVeterinario = cognomeVeterinario;
+    }
+
+    public String getCuraPrescritta() {
+        return curaPrescritta;
+    }
+
+    public void setCuraPrescritta(String curaPrescritta) {
+        this.curaPrescritta = curaPrescritta;
+    }
+
+
+}
---
+++ src/java/com/unife/project/model/mo/Zona.java
@@ -0,0 +1,108 @@
+public class Zona {
+    private int coordX; //PK
+    private int coordY; //PK
+    private int portataSensore;
+    private String statoTerreno;    //stato generale del terreno
+    private float sensoreIluminazione;
+    private float sensoreUmidita;
+    private float sensoreTemperatura;
+    private float sensorePH;
+    private float sensoreVento;
+    private ind id_piantagione; //FK di piantagione
+
+    public Zona(int coordX, int coordY, int portataSensore, String statoTerreno, float sensoreIluminazione, float sensoreUmidita, float sensoreTemperatura, float sensorePH, float sensoreVento, ind id_piantagione) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.portataSensore = portataSensore;
+        this.statoTerreno = statoTerreno;
+        this.sensoreIluminazione = sensoreIluminazione;
+        this.sensoreUmidita = sensoreUmidita;
+        this.sensoreTemperatura = sensoreTemperatura;
+        this.sensorePH = sensorePH;
+        this.sensoreVento = sensoreVento;
+        this.id_piantagione = id_piantagione;
+    }
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+
+    public int getPortataSensore() {
+        return portataSensore;
+    }
+
+    public void setPortataSensore(int portataSensore) {
+        this.portataSensore = portataSensore;
+    }
+
+    public String getStatoTerreno() {
+        return statoTerreno;
+    }
+
+    public void setStatoTerreno(String statoTerreno) {
+        this.statoTerreno = statoTerreno;
+    }
+
+    public float getSensoreIluminazione() {
+        return sensoreIluminazione;
+    }
+
+    public void setSensoreIluminazione(float sensoreIluminazione) {
+        this.sensoreIluminazione = sensoreIluminazione;
+    }
+
+    public float getSensoreUmidita() {
+        return sensoreUmidita;
+    }
+
+    public void setSensoreUmidita(float sensoreUmidita) {
+        this.sensoreUmidita = sensoreUmidita;
+    }
+
+    public float getSensoreTemperatura() {
+        return sensoreTemperatura;
+    }
+
+    public void setSensoreTemperatura(float sensoreTemperatura) {
+        this.sensoreTemperatura = sensoreTemperatura;
+    }
+
+    public float getSensorePH() {
+        return sensorePH;
+    }
+
+    public void setSensorePH(float sensorePH) {
+        this.sensorePH = sensorePH;
+    }
+
+    public float getSensoreVento() {
+        return sensoreVento;
+    }
+
+    public void setSensoreVento(float sensoreVento) {
+        this.sensoreVento = sensoreVento;
+    }
+
+    public ind getId_piantagione() {
+        return id_piantagione;
+    }
+
+    public void setId_piantagione(ind id_piantagione) {
+        this.id_piantagione = id_piantagione;
+    }
+
+    
+
+}

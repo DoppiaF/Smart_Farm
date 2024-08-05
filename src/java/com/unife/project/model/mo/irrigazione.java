5e51d99f77ad13e4ee88ca2b54f51ea306269161
Francesco Furini <francesco.furini@edu.unife.it>
2024-08-05 13:11:20 +0200

    aggiunto model object 'irrigazione' che mappa la tabella mysql

---
+++ src/java/com/unife/project/model/mo/irrigazione.java
@@ -0,0 +1,82 @@
+package com.unife.project.model.mo;
+
+import java.time.LocalDateTime;
+
+public class irrigazione {
+    private int id_impostazione;
+    private LocalDateTime data_inizio;
+    private int durata;
+    private boolean auto;
+    private String stato;
+    private int litri_usati;
+
+    public irrigazione(
+            int id_impostazione, 
+            LocalDateTime data_inizio, 
+            int durata, 
+            boolean auto, 
+            String stato, 
+            int litri_usati) {
+
+
+        this.id_impostazione = id_impostazione;
+        this.data_inizio = data_inizio;
+        this.durata = durata;
+        this.auto = auto;
+        this.stato = stato;
+        this.litri_usati = litri_usati;
+    }
+
+    public int getId_impostazione() {
+        return id_impostazione;
+    }
+
+    public void setId_impostazione(int id_impostazione) {
+        this.id_impostazione = id_impostazione;
+    }
+
+    public LocalDateTime getData_inizio() {
+        return data_inizio;
+    }
+
+    public void setData_inizio(LocalDateTime data_inizio) {
+        this.data_inizio = data_inizio;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    public boolean isAuto() {
+        return auto;
+    }   
+
+    public void setAuto(boolean auto) {
+        this.auto = auto;
+    }
+
+    public String getStato() {
+        return stato;
+    }
+
+    public void setStato(String stato) {
+        this.stato = stato;
+    }
+
+    public int getLitri_usati() {
+        return litri_usati;
+    }
+
+    public void setLitri_usati(int litri_usati) {
+        this.litri_usati = litri_usati;
+    }
+
+    
+
+
+
+}

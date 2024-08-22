2767780b5856116e899f9bc0d83749d7543d3224
Francesco Furini <francesco.furini@edu.unife.it>
2024-08-22 22:31:59 +0200

    primo test: verifica connessione jdbc

--- src/test/java/com/unife/project/util/DatabaseConnectionTest.java
+++ src/test/java/com/unife/project/util/DatabaseConnectionTest.java
@@ -2,19 +2,24 @@ package com.unife.project.util;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 public class DatabaseConnectionTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void testDatabaseConnection() {
         try {
             Connection connection = DatabaseConnection.getConnection();
+            assertNotNull("Connessione al database fallita!", connection);
             if (connection != null) {
                 System.out.println("Connessione al database riuscita!");
                 connection.close();
-            } else {
-                System.out.println("Connessione al database fallita!");
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            // Puoi anche aggiungere un'asserzione per fallire il test in caso di eccezione
+            org.junit.Assert.fail("Eccezione durante la connessione al database: " + e.getMessage());
         }
     }
 }

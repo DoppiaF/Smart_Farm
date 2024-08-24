0fb1cd0e7375d474da1d47104e855febd3b4209f
Francesco Furini <francesco.furini@edu.unife.it>
2024-08-24 13:12:40 +0200

    aggiunto metodo save a irrigazioneDAOimpl

--- src/main/java/com/unife/project/model/dao/IrrigazioneDAOImpl.java
+++ src/main/java/com/unife/project/model/dao/IrrigazioneDAOImpl.java
@@ -1,10 +1,15 @@
 package com.unife.project.model.dao;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
 import com.unife.project.model.mo.Irrigazione;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
 
 public class IrrigazioneDAOImpl implements IrrigazioneDAO {
     private List<Irrigazione> irrigazioni = new ArrayList<>();  //lista da riempire coi dati raccolti dal database, passare poi a return
@@ -19,7 +24,23 @@ public class IrrigazioneDAOImpl implements IrrigazioneDAO {
 
     @Override
     public void save(Irrigazione irrigazione) {
-        
+        String sql = "INSERT INTO irrigazione (data_inizio, ora_inizio, durata, auto, stato, litri_usati) VALUES (?,?,?,?,?,?)";    
+
+        try{PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(irrigazione.getData_inizio()));
+            ps.setTime(2, Time.valueOf(irrigazione.getOra_inizio()));
+            ps.setInt(3, irrigazione.getDurata());
+            ps.setBoolean(4, irrigazione.isAuto());
+            ps.setString(5, irrigazione.getStato());
+            ps.setInt(6, irrigazione.getLitri_usati());
+            int rowsInserted = ps.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Una nuova irrigazione è stata inserita correttamente!");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Errore nell'inserimento di un'irrigazione");
+        }
     }
 
     @Override

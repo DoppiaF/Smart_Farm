58f4e38ed2fdbe2d0b327fb59919984b279170e2
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-02 10:33:59 +0200

    aggiunto irrigazioneCisternaDAO

---
+++ src/main/java/com/unife/project/model/dao/IrrigazioneCisternaDAO.java
@@ -0,0 +1,7 @@
+package com.unife.project.model.dao;
+
+import com.unife.project.model.mo.IrrigazioneCisterna;
+
+public interface IrrigazioneCisternaDAO extends GenericDAO<IrrigazioneCisterna>{
+    //metodi specifici per irrigazioneCisterna
+}

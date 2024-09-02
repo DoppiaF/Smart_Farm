fb5d0baaa1d84be3df71b3f326e49587ddd6ccba
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-02 12:53:38 +0200

    ListinoDAOImpl e ListinoDAO completati

--- src/main/java/com/unife/project/model/dao/ListinoDAO.java
+++ src/main/java/com/unife/project/model/dao/ListinoDAO.java
@@ -1,7 +1,10 @@
 package com.unife.project.model.dao;
 
+import java.util.List;
+
 import com.unife.project.model.mo.Listino;
 
 public interface ListinoDAO extends GenericDAO<Listino> {
     //metodi specifici per listino da definire.
+    List<Listino> findByTipoProdotto(String tipo_prodotto);
 }

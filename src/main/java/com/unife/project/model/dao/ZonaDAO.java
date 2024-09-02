8cbd9cde6230aeaa9859b193904f11ac11bfda50
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-02 15:25:02 +0200

    aggiunta zonaDAO

---
+++ src/main/java/com/unife/project/model/dao/ZonaDAO.java
@@ -0,0 +1,15 @@
+package com.unife.project.model.dao;
+
+import com.unife.project.model.mo.Zona;
+
+public interface ZonaDAO extends GenericDAO<Zona> {
+    //metodi specifici per zona
+    //List<Zona> findByNome(String nome);
+    //List<Zona> findByCapienza(int capienza);
+    //List<Zona> findByTipo(String tipo);
+    //List<Zona> findByCapienzaAndTipo(int capienza, String tipo);
+    //List<Zona> findByNomeAndCapienza(String nome, int capienza);
+    //List<Zona> findByNomeAndTipo(String nome, String tipo);
+    //List<Zona> findByNomeAndCapienzaAndTipo(String nome, int capienza, String tipo);
+    
+}

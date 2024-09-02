52296b945209d0b99015a9b4703dcbc2c1aec032
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-02 14:50:00 +0200

    utenteDAOImpl, utente MO e utenteDAO conclusi

---
+++ src/main/java/com/unife/project/model/dao/UtenteDAO.java
@@ -0,0 +1,16 @@
+package com.unife.project.model.dao;
+
+import com.unife.project.model.mo.Utente;
+
+public interface UtenteDAO extends GenericDAO<Utente> {
+    //public Utente findByUsername(String username);
+    //public Utente findByEmail(String email);
+    //public Utente findByUsernameAndPassword(String username, String password);
+    //public Utente findByEmailAndPassword(String email, String password);
+    //public void updatePassword(Utente utente, String newPassword);
+    //public void updateEmail(Utente utente, String newEmail);
+    //public void updateRuoli(Utente utente, Boolean ruolo_raccolta, Boolean ruolo_irrigazione, Boolean ruolo_pastore, Boolean ruolo_admin);
+    //public void updateDataNascita(Utente utente, LocalDate newDataNascita);
+    //public void delete(Utente utente);
+    //public void deleteAll();
+}

29fd2516572aa6054e74222e8df81b27caa82bc6
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-02 15:49:31 +0200

    implementato zonaDAO, zonaDAOImpl e Zona. fine della gestione crud dei DAO

--- src/main/java/com/unife/project/model/dao/ZonaDAOImpl.java
+++ src/main/java/com/unife/project/model/dao/ZonaDAOImpl.java
@@ -3,7 +3,9 @@ package com.unife.project.model.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.ResultSet;
 
 import com.unife.project.model.mo.Zona;
 
@@ -49,7 +51,7 @@ public class ZonaDAOImpl implements ZonaDAO {
 
     @Override
     public void update(Zona zona) {
-        String sql = "UPDATE zona SET portataSensore = ?, statoTerreno = ?, sensoreIlluminazione = ?, sensoreUmidita = ?, sensoreTemperatura = ?, sensorePH = ?, sensoreVento = ?, id_piantagione = ? WHERE coordX = ? and coordY = ?";
+        String sql = "UPDATE zona SET portataSensore = ?, statoTerreno = ?, sensoreIlluminazione = ?, sensoreUmidita = ?, sensoreTemperatura = ?, sensorePH = ?, sensoreVento = ? WHERE id_piantagione = ? and coordX = ? and coordY = ?";
 
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, zona.getPortataSensore());
@@ -76,11 +78,12 @@ public class ZonaDAOImpl implements ZonaDAO {
 
     @Override
     public void delete(Zona zona) {
-        String sql = "DELETE FROM zona WHERE coordX = ? and coordY = ?";
+        String sql = "DELETE FROM zona WHERE coordX = ? and coordY = ? and id_piantagione = ?";
 
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, zona.getCoordX());
             ps.setInt(2, zona.getCoordY());
+            ps.setInt(3, zona.getId_piantagione());
             int rowsDeleted = ps.executeUpdate();
             if (rowsDeleted > 0) {
                 System.out.println("La zona è stata eliminata correttamente!");
@@ -93,17 +96,84 @@ public class ZonaDAOImpl implements ZonaDAO {
     }
 
     @Override
-    public Zona findById(int coordX, int coordY) {
-        // TODO Auto-generated method stub
+    public Zona findById(int id) {
+        // non implementabile
+
+        System.out.println("Metodo non implementabile. utilizzare findByCoordAndIdPiantagione()");
         return null;
     }
 
     @Override
     public List<Zona> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        zone = new ArrayList<Zona>();
+        String sql = "SELECT * FROM zona";
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                if(rs.next() == false){
+                    System.out.println("Non ci sono zone nel database");
+                } else {
+                    while (rs.next()) {
+                        Zona zona = new Zona(
+                            rs.getInt("coordX"),
+                            rs.getInt("coordY"),
+                            rs.getInt("portataSensore"),
+                            rs.getString("statoTerreno"),
+                            rs.getFloat("sensoreIlluminazione"),
+                            rs.getFloat("sensoreUmidita"),
+                            rs.getFloat("sensoreTemperatura"),
+                            rs.getFloat("sensorePH"),
+                            rs.getFloat("sensoreVento"),
+                            rs.getInt("id_piantagione")
+                            );
+
+                        zone.add(zona);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Errore nel recupero delle zone");
+        }
+
+        return zone;
     }
 
-    
+
+    @Override
+    public Zona findByCoordAndPiantagione(int coordX, int coordY, int id_piantagione) {
+        Zona zona = null;
+        String sql = "SELECT * FROM zona WHERE coordX = ? and coordY = ? and id_piantagione = ?";
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, coordX);
+            ps.setInt(2, coordY);
+            ps.setInt(3, id_piantagione);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    zona = new Zona(
+                        rs.getInt("coordX"),
+                        rs.getInt("coordY"),
+                        rs.getInt("portataSensore"),
+                        rs.getString("statoTerreno"),
+                        rs.getFloat("sensoreIlluminazione"),
+                        rs.getFloat("sensoreUmidita"),
+                        rs.getFloat("sensoreTemperatura"),
+                        rs.getFloat("sensorePH"),
+                        rs.getFloat("sensoreVento"),
+                        rs.getInt("id_piantagione")
+                        );
+                } else {
+                    System.out.println("Zona non trovata");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Errore nella ricerca di una zona");
+        }
+
+        return zona;
+    }
 
 }

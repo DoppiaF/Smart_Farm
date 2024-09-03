08b45bdbcdb366e68a1ed29287abe5d40ac53632
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-03 11:37:56 +0200

    javafx funzionante

--- src/main/java/com/unife/project/MainApp.java
+++ src/main/java/com/unife/project/MainApp.java
@@ -1,40 +1,41 @@
 package com.unife.project;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import com.unife.project.controller.LoginController;
 import com.unife.project.model.dao.UtenteDAOImpl;
 import com.unife.project.util.DatabaseConnection;
-import com.unife.project.view.LoginView;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-       try{ // Configura la connessione al database
+        try {
+            // Configura la connessione al database
             Connection connection = DatabaseConnection.getConnection();
             UtenteDAOImpl utenteDAO = new UtenteDAOImpl(connection);
 
-            // Crea la vista
-            LoginView loginView = new LoginView();
+            // Carica il file FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/unife/project/view/login.fxml"));
+            loader.setControllerFactory(c -> new LoginController(utenteDAO));
+            Parent root = loader.load();
 
-            // Crea il controller
-            LoginController loginController = new LoginController(utenteDAO, loginView);
-
-            // Mostra la vista
-            loginView.start(primaryStage);
-            
-        } catch (SQLException e){
+            // Imposta la scena
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Login");
+            primaryStage.show();
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Errore nella connessione al database");
         }
     }
 
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 }

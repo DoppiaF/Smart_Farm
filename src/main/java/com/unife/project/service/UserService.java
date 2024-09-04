e5e8774bb89647537482693d7b3bdb4227bfee68
Alessandro Zerbinati <alessand.zerbinati@edu.unife.it>
2024-09-04 16:38:21 +0200

    aggiunta della cartella service con file UserService

---
+++ src/main/java/com/unife/project/service/UserService.java
@@ -0,0 +1,19 @@
+package com.unife.project.service;
+
+import com.unife.project.model.dao.DAOFactory;
+import com.unife.project.model.mo.Utente;
+import com.unife.project.model.dao.UtenteDAO;
+import com.unife.project.util.DatabaseConnection;
+
+public class UserService {
+
+    public Utente authenticate(String username, String password){
+        UtenteDAO utenteDAO = DAOFactory.getUtenteDAO();
+        
+        Utente utente = utenteDAO.findByUsernameAndPassword(username, password)
+
+        return utente;
+    }
+            
+    
+}

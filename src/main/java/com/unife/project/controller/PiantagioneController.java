34e4483bf52372e050cdf9130d11bfc36148fd44
Alessandro Zerbinati <alessand.zerbinati@edu.unife.it>
2024-09-06 10:39:57 +0200

    Implementazione passaggio utente in sessione da home a piantagionePage

--- src/main/java/com/unife/project/controller/PiantagioneController.java
+++ src/main/java/com/unife/project/controller/PiantagioneController.java
@@ -1,16 +1,28 @@
 package com.unife.project.controller;
 
+import java.io.IOException;
+
 import com.unife.project.model.mo.Piantagione;
 import com.unife.project.model.mo.Utente;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 public class PiantagioneController {
 
     private Utente utente;
+    
+    @FXML
+    private BorderPane piantagioneRoot;
+    
+    @FXML
+    private BorderPane piantagioneNested;
 
     @FXML
     private TableView<Piantagione> piantagioneTable;
@@ -64,6 +76,55 @@ public class PiantagioneController {
     //metodo da chiamare da altri controller per passare l'utente alla home
     public void setUser(Utente utente){
         this.utente = utente;
-        //updateMenuBar();
+        updateMenuBar();
+        updateVerticalMenuBar();
     }
+    
+    private void updateMenuBar(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/unife/project/view/menuBar.fxml"));
+            Parent menuBarRoot = loader.load();
+
+            // Ottieni il controller della barra di menu
+            MenuBarController menuBarController = loader.getController();
+            //passa utente al controller menu bar e aggiorna visibilità bottoni
+            menuBarController.setUserStatus(utente);
+
+            
+            // Aggiungi la barra di menu alla root
+            piantagioneNested.setTop(menuBarRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+            showErrorDialog("Errore", "Impossibile caricare la barra di menu.");
+        }
+    }
+
+    private void updateVerticalMenuBar(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/unife/project/view/verticalMenuBar.fxml"));
+            Parent verticalMenuBarRoot = loader.load();
+
+            // Ottieni il controller della barra di menu
+            VerticalMenuBarController verticalMenuBarController = loader.getController();
+            //passa utente al controller menu bar e aggiorna visibilità bottoni
+            verticalMenuBarController.setUserStatus(utente);
+
+            
+            // Aggiungi la barra di menu alla root
+            piantagioneRoot.setLeft(verticalMenuBarRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+            showErrorDialog("Errore", "Impossibile caricare la barra di menu.");
+        }
+    }
+
+    
+    private void showErrorDialog(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }

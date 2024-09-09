fcd9c3d09707b0e706e83077439dd0a804f5a8d4
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-09 11:59:11 +0200

    aggiunto controller per animali e bottone goTo partito da pastoreController

--- src/main/java/com/unife/project/model/dao/AnimaleDAO.java
+++ src/main/java/com/unife/project/model/dao/AnimaleDAO.java
@@ -1,10 +1,13 @@
 package com.unife.project.model.dao;
 
+import java.util.List;
+
 import com.unife.project.model.mo.Animale;
-//import java.util.List;
+
 
 public interface AnimaleDAO extends GenericDAO<Animale> {
     //List<Animale> findBySpecies(String species);
     //void updateHealthStatus(int id, String healthStatus);
     //List<Animale> findByAge(int age);
+    List<Animale> findByStalla(String etichettaStalla);
 }

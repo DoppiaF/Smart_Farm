f9e44c8f6986f2dc7a1156d2a7277566b37e978b
Francesco Furini <francesco.furini@edu.unife.it>
2024-09-15 18:33:56 +0200

    aggiunte variabili ambiente per connessione db da docker hub

--- src/main/java/com/unife/project/util/DatabaseConnection.java
+++ src/main/java/com/unife/project/util/DatabaseConnection.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+
+    //modifiche per selezionare le variabili d'ambiente docker hub se presenti
     static Connection connection = null;
-    static String databaseName = "fattoria";
-    //static String connectInfo = "?autoReconnect=true&useSSL=false";
-    private static final String URL = "jdbc:mysql://localhost:3306/" + databaseName;// + connectInfo;
-    private static final String USER = "root";
-    private static final String PASSWORD = "Pannocchie98!?";
+    static String databaseName = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "fattoria";
+    private static final String URL = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://localhost:3306/" + databaseName;
+    private static final String USER = System.getenv("DB_USERNAME") != null ? System.getenv("DB_USERNAME") : "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "Pannocchie98!?";
+
 
     static {
         try {
